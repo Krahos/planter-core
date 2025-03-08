5704ba4961337a3e51949d03d1b65f644131238c
Sebastiano Giordano <46520354+Krahos@users.noreply.github.com>
2025-03-08 15:13:59 +0100

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Rust devshell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            bacon
+            cargo-udeps
+            rust-analyzer
+            cargo-deny
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+//! This is a library which contains the core types and behaviour for the PlanTer project management application.
